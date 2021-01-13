2341d1fcfd809bff343ebb2059db65398e7d86ce
New-TW <wlsxodnjs1@gmail.com>
2021-01-13 16:01:01 +0900

    On_off_current

--- run.py
+++ run.py
@@ -1,1 +1,37 @@
-import csv
+import csv as cv
+import pandas as pd
+import numpy as np
+from setup_files import parameters as parameters
+from matplotlib import pyplot as plt
+
+csv_name = [
+    '200_1',
+    '200_20v_1',
+    '200_20v_2',
+    '250_1',
+    '250_2',
+    '250_3',
+    '250_20v',
+    '250_20v_2',
+    '300_1',
+    '300_2',
+    '300_20v_1',
+    'as depo_1',
+    'as depo_20v_1',
+
+            ]
+for csv in csv_name:
+
+    ld = pd.read_csv('./setup_files./data/' + csv +'.csv',engine='python',header=2)
+    gateV = pd.DataFrame(ld, columns=['Gate Vg (V)'])
+    drainI = pd.DataFrame(ld, columns=['Drain Id (A)'])
+    plt.title(csv,fontsize=25)
+    plt.xlabel("Gate V")
+    plt.ylabel("Drain I")
+    plt.plot(gateV,drainI)
+    print(drainI)
+    min_i = drainI.max()
+    max_i = drainI.min()
+    parameters.on_off_current_ratio(max_i,min_i)
+    plt.show()
+    
--- setup_files/parameters.py
+++ setup_files/parameters.py
@@ -1,22 +1,33 @@
+import csv
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
 def subthreshold_swing():
 
     return
 
-def saturation_mobility():
-    L =
-    W =
-    Vgs = # csv 임포트
-    Vth = # Threshold 계산
+def saturation_mobility(Vgs,Vth,i):
+    L =50*10E-6
+    W =1000*10E-6
+    Vgs = 1# csv 임포트
+    Vth = 1 # Threshold 계산
+    if i ==1 :
+        Id = 20
+    else:
+        Id = 40
+
     gm = 2*Id/(Vgs-Vth)
     Ci = 17 * 10E-9 #F
-    Vds = # 변수
+    Vds = 1 # 변수
     mu_sat = (L*gm)/(W*Ci*Vds)
 
     return mu_sat
 
-def on_off_current_ratio():
-
-    return
+def on_off_current_ratio(on,off):
+    on_off_current_ratio = on / off
+    print(float(on_off_current_ratio))
+    return  on_off_current_ratio
 
 def threshold_volatge():
 
