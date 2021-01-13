de32ed158e15378acc54f5287c6b95567f6f8955
New-TW <wlsxodnjs1@gmail.com>
2021-01-13 13:29:43 +0900

    first

---
+++ setup_files/parameters.py
@@ -0,0 +1,24 @@
+def subthreshold_swing():
+
+    return
+
+def saturation_mobility():
+    L =
+    W =
+    Vgs = # csv 임포트
+    Vth = # Threshold 계산
+    gm = 2*Id/(Vgs-Vth)
+    Ci = 17 * 10E-9 #F
+    Vds = # 변수
+    mu_sat = (L*gm)/(W*Ci*Vds)
+
+    return mu_sat
+
+def on_off_current_ratio():
+
+    return
+
+def threshold_volatge():
+
+    return
+
